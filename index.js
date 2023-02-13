26347c4cb1f96539e5df6da501c03b7541fc5b25
TypicalTeacup <pskirel@wp.pl>
2023-02-13 17:45:55 +0000

    fixing font

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
-const { createCanvas,GlobalFonts } = require("@napi-rs/canvas");
+const { createCanvas, GlobalFonts } = require("@napi-rs/canvas");
 
-GlobalFonts.registerFromPath("font.ttf","impact")
-
+GlobalFonts.registerFromPath(__dirname + "/font.ttf", "impact");
 
 const headerText = "Math challenge (99% fail):";
 
@@ -17,23 +16,26 @@ app.get("/mathchallenge", (request, result) => {
     const ctx = canvas.getContext("2d");
 
     ctx.font = `45px impact`;
-    ctx.lineWidth = 2
+    ctx.lineWidth = 2;
     ctx.fillStyle = `white`;
-    const headerX = canvasWidth/2 - (ctx.measureText(headerText).width/2);
+    const headerX = canvasWidth / 2 - ctx.measureText(headerText).width / 2;
     ctx.strokeText(headerText, headerX, 60);
 
-    ctx.font = `90px impact`
-    ctx.lineWidth = 3
-    const num1 = Math.floor(Math.random()*20)+10
+    ctx.font = `90px impact`;
+    ctx.lineWidth = 3;
+    const num1 = Math.floor(Math.random() * 20) + 10;
     let dividers = [];
     for (let i = 2; i < num1; i++) {
-        if (num1%1==0) dividers.push(i)
+        if (num1 % 1 == 0) dividers.push(i);
     }
-    const num2 = dividers.length == 0 ? 1 : dividers[Math.floor(Math.random()*dividers.length)]
-    const num3 = Math.floor(Math.random()*30)+1
-    const equation = `${num1} / ${num2} + ${num3}`
+    const num2 =
+        dividers.length == 0
+            ? 1
+            : dividers[Math.floor(Math.random() * dividers.length)];
+    const num3 = Math.floor(Math.random() * 30) + 1;
+    const equation = `${num1} / ${num2} + ${num3}`;
 
-    const equationX = canvasWidth/2 - (ctx.measureText(equation).width/2);
+    const equationX = canvasWidth / 2 - ctx.measureText(equation).width / 2;
     ctx.strokeText(equation, equationX, 160);
 
     result
@@ -47,4 +49,4 @@ app.listen(80, () => {
     console.log("math challenge is running");
 });
 
-module.exports = app
+module.exports = app;
